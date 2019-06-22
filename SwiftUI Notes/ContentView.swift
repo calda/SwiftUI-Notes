bfcc1551e4f55ef48846ab2ada548e80e01da6f8
Cal Stephens <cal@calstephens.tech>
2019-06-22 13:50:28 -0400

    Initial Commit

---
+++ SwiftUI Notes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI Notes
+//
+//  Created by Cal Stephens on 6/22/19.
+//  Copyright © 2019 Cal Stephens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
